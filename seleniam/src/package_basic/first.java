0a07feaf1eeead9300166221a0c4282d1e1a2946
narasimha123616 <Kuruvanarasimha569@gmail.com>
2025-08-23 10:42:04 +0530

    selenuim driver

---
+++ seleniam/src/package_basic/first.java
@@ -0,0 +1,22 @@
+package package_basic;
+
+
+
+public class first {
+String name="Narasimha";
+int rollno=10;
+float per=95.8f;
+int age=23;
+String course="Java selenium";
+char Add='K';
+
+public static void main(String[] args) {
+	first nara=new first();
+	System.out.println("name      :"+nara.name);
+	System.out.println("rollno    :"+nara.rollno);
+	System.out.println("per       :"+nara.per);
+	System.out.println("age       :"+nara.age);
+	System.out.println("course    :"+nara.course);
+	System.out.println("Add       :"+nara.Add);
+}
+}
